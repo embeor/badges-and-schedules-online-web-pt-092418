a4473822467745976326afe107228989c372ee87
Emilio Betancourt <embeor@gmail.com>
2018-10-01 00:17:30 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (attendees)
+  names_badge = []
+  attendees.each do |attendee|
+  end
+end
+
+def assign_rooms (attendees)
+  attendees. each do |attendee|
+  end
+end
+
+def printer
+
+end
