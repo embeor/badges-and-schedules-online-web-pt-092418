dc3e4a088b2f6d386b6dbd216bb001650fd8ea1b
Emilio Betancourt <embeor@gmail.com>
2018-10-02 03:35:58 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,15 +4,29 @@ end
 
 def batch_badge_creator (attendees)
   names_badge = []
-  attendees.each do |attendee|
-  end
+    attendees.each do|attendee|
+      names_badge << "Hello, my name is #{attendee}."
+    end
+  names_badge
 end
 
 def assign_rooms (attendees)
-  attendees. each do |attendee|
-  end
+  names_and_room = []
+  room = 1
+    attendees. each do |attendee|
+      names_and_room << "Hello, #{attendee}! You'll be assigned to room #{room}!"
+      room +=1
+    end
+  names_and_room
 end
 
-def printer
-
+def printer (attendees)
+  attendees.each do |attendees|
+    puts "Hello, my name is #{attendees}."
+  end
+    room = 1
+    attendees. each do |attendee|
+      puts "Hello, #{attendee}! You'll be assigned to room #{room}!"
+      room +=1
+    end
 end
